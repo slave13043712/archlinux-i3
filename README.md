d9f457b7af2f661938f80d0217644d0850b0f82b
slave13043712 <slave13043712@users.noreply.github.com>
2016-07-13 22:54:09 +0000

    Updated README.md File

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Archlinux Instance with i3 Window Manager
+# Arch Linux Instance with i3 Window Manager
 
 ## Overview
 This project provides a set of install scripts to install [Arch Linux](https://www.archlinux.org/) with [i3 window manager](https://i3wm.org/).
--- install_home.sh
+++ install_home.sh
@@ -8,4 +8,9 @@ cp .Xresources ~/
 cp .i3status.conf ~/
 cp -R .i3 ~/
 mkdir ~/repositories ~/Downloads ~/Images
+# set some basic git config settings
+git config --global core.editor vim
+git config --global core.eol lf
+#git config --global user.name <your name>
+#git config --global user.email <your email>
 
